1dea9ce6e929bd3826a16c39062c48447df7df87
David Dean <vermicious.knid.d@gmail.com>
2024-09-11 13:47:13 -0700

    Changes port to 8080. Updates example at bottom.

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ import chatCompletion from "./chatCompletion.js"
 
 // Express
 const app = express()
-const port = 3030
+const port = 8080
 // Use CORS
 app.use(cors())
 // Disable X-Powered-By: Express for security reasons
@@ -33,4 +33,4 @@ app.listen(port, () => {
 })
 
 // How to invoke text to image
-// http://192.168.0.33:3030/a%20mountain%20landscape%20with%20a%20snow%20covered%20mountain%20in%20the%20distance%20a%20sunrice%20behind%20it/3d,%20cartoon,%20anime,%20(deformed%20eyes,%20nose,%20ears,%20nose),%20bad%20anatomy,%20ugly,%20blurry/8.5
+// http://192.168.0.33:8080/image/a%20mountain%20landscape%20with%20a%20snow%20covered%20mountain%20in%20the%20distance%20a%20sunrice%20behind%20it/3d,%20cartoon,%20anime,%20(deformed%20eyes,%20nose,%20ears,%20nose),%20bad%20anatomy,%20ugly,%20blurry/8.5
