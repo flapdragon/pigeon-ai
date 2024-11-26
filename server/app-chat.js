cd338fb453dd48af732332391c9eef3633dcb3c0
David Dean <vermicious.knid.d@gmail.com>
2024-11-25 19:51:02 -0700

    Splits chat and image into 2 separate servers.

--- server/app-chat.js
+++ server/app-chat.js
@@ -2,14 +2,13 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
-import textToImage from "./textToImage.js"
 import chatCompletion from "./chatCompletion.js"
 
 // TODO: Make Express wait on connection to MongoDB
 
 // Express
 const app = express()
-const port = 8080
+const port = 8082
 // Use CORS
 app.use(cors())
 // Disable X-Powered-By: Express for security reasons
@@ -23,14 +22,14 @@ const mainDB = async () => {
 }
 mainDB().catch((err) => console.log(err))
 
-// Image routes
-app.use("/image", textToImage)
 // Chat routes
 app.use("/chat", chatCompletion)
+// Image route for no
+app.get("/image", (req, res) => res.status(200).send("(╯°□°)╯︵ ┻━┻ No!!! Go to http://192.168.1.100:8081/image"))
 
 app.listen(port, () => {
   console.log(`Image maker app listening on port ${port}`)
 })
 
-// How to invoke text to image
-// http://192.168.0.33:8080/image/a%20mountain%20landscape%20with%20a%20snow%20covered%20mountain%20in%20the%20distance%20a%20sunrice%20behind%20it/3d,%20cartoon,%20anime,%20(deformed%20eyes,%20nose,%20ears,%20nose),%20bad%20anatomy,%20ugly,%20blurry/8.5
+// How to invoke chat ai
+// http://192.168.0.100:8082/image/a%20mountain%20landscape%20with%20a%20snow%20covered%20mountain%20in%20the%20distance%20a%20sunrice%20behind%20it/3d,%20cartoon,%20anime,%20(deformed%20eyes,%20nose,%20ears,%20nose),%20bad%20anatomy,%20ugly,%20blurry/8.5
--- server/textToImage.js
+++ server/textToImage.js
@@ -7,52 +7,57 @@ import { images } from "./imagesModel.js"
 // TODO: Automate archiving of images (yearly? monthly?)
 
 // Get Hugging Face token
-const HF_TOKEN = process.env.HF_TOKEN || ""
+const HF_TOKEN = process.env.HF_TOKEN_IMAGE || ""
 
 const hf = new HfInference(HF_TOKEN)
 
 const textToImage = express.Router()
 
 textToImage.get('/:inputs/:negative_prompt/:guidance_scale', async (req, res) => {
-  // Get user ip
-  const clientIPArray = req.ip.split(":")
-  const ip = clientIPArray[clientIPArray.length-1]
-  // Get params
-  const { inputs, negative_prompt, guidance_scale } = req.params
-  // Log user ips and requests
-  console.log(ip, `${inputs.substring(0, 10)}...`)
+  try {
+    // Get user ip
+    const clientIPArray = req.ip.split(":")
+    const ip = clientIPArray[clientIPArray.length-1]
+    // Get params
+    const { inputs, negative_prompt, guidance_scale } = req.params
+    // Log user ips and requests
+    console.log(ip, `${inputs.substring(0, 10)}...`)
 
-  // Hard code model
-  const model = "stabilityai/stable-diffusion-xl-base-1.0"
-  // TODO: check for inappropriate inputs
-  // TODO: need param validation
-  const blob = await hf.textToImage({
-    model: model,
-    inputs: inputs,
-    parameters: {
-      negative_prompt: negative_prompt,
-      guidance_scale: parseFloat(guidance_scale)
-    }
-  })
-  // Convert Hugging Face response blob to buffer
-  const buffer = await blob.arrayBuffer()
-  // Get blob file extension
-  const fileExt = blob.type.split('/')[1]
-  // New file name
-  const filename = `hf_${ip}_${Date.now().toString()}.${fileExt}`
-  // Path, save to images folder
-  const filePath = resolve(join(`${process.cwd()}/images`, filename))
-  // Save file
-  await writeFile(filePath, Buffer.from(buffer))
+    // Hard code model
+    const model = "stabilityai/stable-diffusion-xl-base-1.0"
+    // TODO: check for inappropriate inputs
+    // TODO: need param validation
+    const blob = await hf.textToImage({
+      model: model,
+      inputs: inputs,
+      parameters: {
+        negative_prompt: negative_prompt,
+        guidance_scale: parseFloat(guidance_scale)
+      }
+    })
+    // Convert Hugging Face response blob to buffer
+    const buffer = await blob.arrayBuffer()
+    // Get blob file extension
+    const fileExt = blob.type.split('/')[1]
+    // New file name
+    const filename = `hf_${ip}_${Date.now().toString()}.${fileExt}`
+    // Path, save to images folder
+    const filePath = resolve(join(`${process.cwd()}/images`, filename))
+    // Save file
+    await writeFile(filePath, Buffer.from(buffer))
 
-  console.log(ip, model, inputs, negative_prompt, guidance_scale, filename)
-  // Create image log record
-  // NOTE: Uses hard-coded model for now, until a UI gets made that passes it in
-  await images.create({
-    ip, model, inputs, negative_prompt, guidance_scale, filename
-  })
-  // Respond with file
-  res.sendFile(filePath)
+    console.log(ip, model, inputs, negative_prompt, guidance_scale, filename)
+    // Create image log record
+    // NOTE: Uses hard-coded model for now, until a UI gets made that passes it in
+    await images.create({
+      ip, model, inputs, negative_prompt, guidance_scale, filename
+    })
+    // Respond with file
+    res.sendFile(filePath)
+  }
+  catch (err) {
+     res.send("(╯°□°)╯︵ ┻━┻<br />Error: Max requests total reached on image generation inference (3). Wait up to one minute before being able to process more Diffusion requests.")
+  }
 })
 
 export default textToImage
